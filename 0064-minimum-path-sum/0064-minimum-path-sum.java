f3da4568465d34f9ff1b900c58effc69d60a9b55
KARTIKEYA RAWAT <153120062+KARTIKEYARAWAT@users.noreply.github.com>
2026-06-26 16:48:18 +0530

    Time: 5 ms (26.14%), Space: 50.3 MB (54.87%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -13,24 +13,28 @@ class Solution {
         int n=grid.length;
         int m=grid[0].length;
 
-        int[][]dp=new int[n][m];
+       // int[][]dp=new int[n][m];
+       int[] prev=new int[m];
         for(int i=0;i<n;i++){
+            int[]curr=new int[m];
             for(int j=0;j<m;j++){
-                 if(i==0 &&j==0){ dp[i][j]=grid[0][0];continue;}
-                 int down=(int)1e9;
-                 int right=(int)1e9;
+                 //if(i==0 &&j==0){ dp[i][j]=grid[0][0];continue;}
+                 if(i==0 &&j==0){ curr[j]=grid[0][0];continue;}
+                 int up=(int)1e9;
+                 int left=(int)1e9;
                  if(i>0){
-                     down=grid[i][j] + dp[i-1][j];
+                     up=grid[i][j] + prev[j];
                  }
                  if(j>0){
-                    right=grid[i][j]+dp[i][j-1];
+                    left=grid[i][j]+curr[j-1];
                  }
-                  dp[i][j]=Math.min(down,right);
-            }
+                  curr[j]=Math.min(up,left);
+            }prev=curr;
           
 
         }
-          return dp[n-1][m-1];
+          //return dp[n-1][m-1];
+          return prev[m-1];
     //     for(int[]row : dp){
     //     Arrays.fill(row,-1);}
     //     return solve(n-1,m-1,grid,dp);
